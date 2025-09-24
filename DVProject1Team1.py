87592172b93be762f7a6b7c679920d707d395fe9
Khoamelo <ckn2004@MSI.localdomain>
2025-09-23 20:17:44 -0700

    Generated scatter plot matrix to display 4 stress level factors vs. stress level

--- DVProject1Team1.py
+++ DVProject1Team1.py
@@ -3,4 +3,14 @@ from sklearn.preprocessing import MinMaxScaler
 import pandas as pd
 
 df = pd.read_csv('StressLevelDataset.csv')
-print(df)
+
+fig = px.scatter_matrix(df, 
+                            dimensions=["anxiety_level", "self_esteem", "depression", "sleep_quality"],
+                            color="stress_level",
+                            height=800,
+                            width=800,
+                            title="Scatter matrix of stress level vs. several related factors"
+                        )
+
+fig.write_html("scatter_matrix.html")
+#print(df)
